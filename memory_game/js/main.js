edc3b2733bd05e5c5d829d3cc846202039f04d98
David Lance <david6116@yahoo.com>
2020-04-14 15:29:34 -0400

    added some control flow logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,18 @@
 
 
-let cardOne = "queen";
-let cardTwo = "queen";
-let cardThree = "king";
-let cardFour = "king";
+const cards = ["queen", "queen", "king", "king"];
+const cardsInPlay = [];
 
-console.log("User flipped "  + cardOne);
-console.log("User flipped "  + cardThree);
+cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("User flipped " + cardsInPlay[0]);
+
+cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardsInPlay[1]);
+
+if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+  alert("You found a match!");
+} else {
+  alert("Sorry, try again.");
+}
